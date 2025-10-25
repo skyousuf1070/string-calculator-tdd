c046b2c987d9ad2013da6956c1a28a2d6fca13ec
yousuf <skyousuf1070@gmail.com>
2025-10-25 16:10:56 +0530

    Revert "feat: Return the sum of two numbers separated by comma"
    This reverts commit a9238dead144ab080d82dd2bb5431ade2ffdb94b.

--- src/main/java/org/calculator/StringCalculator.java
+++ src/main/java/org/calculator/StringCalculator.java
@@ -2,14 +2,9 @@ package org.calculator;
 
 public class StringCalculator {
     public int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
+        if (!numbers.isEmpty()) {
+            return Integer.parseInt(numbers);
         }
-        int sum = 0;
-        String[] inputNumbers = numbers.split(",");
-        for (String number : inputNumbers) {
-            sum += Integer.parseInt(number);
-        }
-        return sum;
+        return 0;
     }
 }
