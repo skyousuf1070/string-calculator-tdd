54bfa4435c2db4bf028e0186702f29c6ec1ded59
yousuf <skyousuf1070@gmail.com>
2025-10-25 16:18:59 +0530

    feat: Return the sum of two numbers separated by comma

--- src/main/java/org/calculator/StringCalculator.java
+++ src/main/java/org/calculator/StringCalculator.java
@@ -2,9 +2,14 @@ package org.calculator;
 
 public class StringCalculator {
     public int add(String numbers) {
-        if (!numbers.isEmpty()) {
-            return Integer.parseInt(numbers);
+        if (numbers.isEmpty()) {
+            return 0;
         }
-        return 0;
+        String[] inputNumbers = numbers.split(",");
+        int sum = Integer.parseInt(inputNumbers[0]);
+        if (inputNumbers.length > 1) {
+            sum += Integer.parseInt(inputNumbers[1]);
+        }
+        return sum;
     }
 }
--- src/test/java/org/calculator/StringCalculatorTest.java
+++ src/test/java/org/calculator/StringCalculatorTest.java
@@ -1,19 +1,29 @@
 package org.calculator;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new StringCalculator();
+    }
+
     @Test
     public void shouldReturnZeroWhenTheNumbersAreEmpty() {
-        StringCalculator calculator = new StringCalculator();
         Assertions.assertEquals(0, calculator.add(""));
     }
 
     @Test
     public void shouldReturnTheSameNumberWhenOnlyOneNumberIsPassed() {
-        StringCalculator calculator = new StringCalculator();
         Assertions.assertEquals(5, calculator.add("5"));
     }
 
+    @Test
+    public void shouldReturnTheSumOfTwoNumbersWhenTwoNumbersArePassedByCommaSeparated() {
+        Assertions.assertEquals(8, calculator.add("2,6"));
+    }
 }
